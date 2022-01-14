209354c499b3d506a1ff435b457450b771a5fb12
Mary Jonah <mary.jonah@turntabl.io>
2022-01-14 19:37:34 +0000

    Start test on CodewarsPollingServiceTest

--- src/test/java/io/turntabl/leaderboardservice/service/CodewarsPollingServiceTest.java
+++ src/test/java/io/turntabl/leaderboardservice/service/CodewarsPollingServiceTest.java
@@ -1,5 +1,15 @@
 package io.turntabl.leaderboardservice.service;
 
+import io.turntabl.leaderboardservice.client.CodewarsClient;
+import io.turntabl.leaderboardservice.converter.UserDtoToProfileConverter;
+import org.mockito.Mock;
+
 class CodewarsPollingServiceTest {
+
+    @Mock
+    private CodewarsClient codewarsClient;
+    private UserDtoToProfileConverter userDtoToProfileConverter;
+
+    private LeaderboardRepositoryService underTest;
 
 }
