96849e733dc218e8464244baf9fa4724997c88d1
Mary Jonah <mary.jonah@turntabl.io>
2022-01-20 08:25:35 +0000

    Include swagger documentation

--- src/main/java/io/turntabl/leaderboardservice/LeaderboardServiceApplication.java
+++ src/main/java/io/turntabl/leaderboardservice/LeaderboardServiceApplication.java
@@ -1,11 +1,14 @@
 package io.turntabl.leaderboardservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+//@OpenAPIDefinition(info = @Info(title = "CodeWars API",version="1.0", description = "Codewars Service here"))
+@OpenAPIDefinition()
 @EnableScheduling
 public class LeaderboardServiceApplication {
 
